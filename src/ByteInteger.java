f7a06530b1f77cc9950fa6f69dbcfc2bf2ed8e03
unknown <JoseAmandiodeAlmeida021099@gmail.com>
2022-06-16 20:59:37 +0900

    Update Integer Byte

---
+++ src/ByteInteger.java
@@ -0,0 +1,13 @@
+public class ByteInteger {
+
+    public static void main(String[] args) {
+
+//        This I sonly Use <= 100 not Max
+
+
+        byte init = 100;
+
+        System.out.println(init);
+
+    }
+}
